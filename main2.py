3865106dd345ccc71bebef62d38e6b7573e83c42
Romaaaaaaan <roma.super2002@ukr.net>
2023-11-28 17:33:54 +0200

    this is main file for lab5

---
+++ main2.py
@@ -0,0 +1,26 @@
+'''
+імпортує класи з файлу lab6
+'''
+from lab6 import Planet, PlanetType, Planetary
+
+def println():
+    '''
+    виводить значення про планети
+    '''
+    planet1 = Planet("Земля", 16, 1000, 30, 24, 9, PlanetType.TERRESTRIAL)
+    planet2 = Planet("Марс", 12, 2000, 10, 20, 1, PlanetType.JOVIAN)
+    planet3 = Planet("Юпітер", 8, 1500, 2, 26, 7, PlanetType.TERRESTRIAL)
+
+    planetary = Planetary()
+    planetary.add_planet(planet1)
+    planetary.add_planet(planet2)
+    planetary.add_planet(planet3)
+
+    print(planetary.find_distance_between(planet1, planet3))
+
+    print(planetary.find_average_mass())
+
+    planetary.sort_by_length_of_day()
+
+if __name__ == "__main__":
+    println()
